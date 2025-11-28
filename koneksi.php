9ece23e25c3da90e96f2d19a33b937badef187d9
Icha Marisa Mahmuda <chaloeymae26@gmail.com>
2025-11-28 12:18:37 +0700

    Add koneksi

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * koneksi.php
+ * File koneksi database menggunakan MySQLi procedural
+ */
+
+// Koneksi ke database db_profil
+$DB = mysqli_connect("localhost", "root", "", "db_profil");
+
+// Cek koneksi
+if (!$DB) {
+    die("Gagal koneksi ke database: " . mysqli_connect_error());
+}
+
+// Set charset UTF-8
+mysqli_set_charset($DB, "utf8");
+?>
